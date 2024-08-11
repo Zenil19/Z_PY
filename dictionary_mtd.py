8faee407054266dd12d6d60c9d6d14a7fae16478
Zenil19 <bca2022zenil1821@tnraocollege.org>
2024-08-11 22:41:53 +0530

    Add files via upload

---
+++ dictionary_mtd.py
@@ -0,0 +1,16 @@
+emp = {
+    'id': 1,
+    'name': 'Zenil Damor',
+    'designation': 'Manager',
+    'salary': 60000
+}
+
+print("Dictionary:",emp)
+
+# Delete the 'designation' key
+del emp['designation']
+print("\nDeleting Designation:",emp)
+
+# Update the 'name' field
+emp['name'] = 'Yash Patel'
+print("\nUpdating 'name':",emp)
